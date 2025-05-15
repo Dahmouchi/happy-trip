a7934ebea385be52cb9f3d458729440b0a2db264
ybouyzem <bouyzemyounes2@gmail.com>
2025-05-15 21:29:54 +0100

    update: in the Hero.tsx and Search.tsx

--- src/app/(landing)/_components/Hero.tsx
+++ src/app/(landing)/_components/Hero.tsx
@@ -4,14 +4,19 @@ import { SearchInput } from './Search'
 const Hero = () => {
   return (
     <div className='lg:p-6 p-2'>
-        <div className=' w-full lg:h-[70vh] h-[40vh] bg-[#8EBD22] rounded-xl bg-cover bg-center relative flex items-center justify-center' style={{backgroundImage:"url('/hero.webp')"}} >
+        <div className=' w-full lg:h-[70vh] h-[40vh] bg-[#8EBD22] rounded-xl shadow-xl/20 bg-cover bg-center relative flex items-center justify-center' style={{backgroundImage:"url('/hero.webp')"}} >
         <div className='text-white text-center z-10 space-y-4'>
             <h1 className='lg:text-5xl text-2xl font-medium tracking-widest'>L&apos;AVENTURE</h1>
             <h1 className='lg:text-7xl text-5xl font-black'>COMMENCE ICI</h1>
             <h1 className='lg:text-lg tracking-wide font-semibold'>Découvrez nos programmes HAPPY TRIP</h1>
-            <SearchInput />
+            <SearchInput/>
         </div>
         <div className='w-full h-full rounded-xl bg-black/20 absolute top-0 z-0'></div>
+        {/* <a href="#next-section" className='absolute bottom-[-25px] left-1/2 transform -translate-x-1/2 w-22 h-22 bg-gradient-to-r from-[#A8D653] to-[#94C93A] rounded-full border-[#A8D653] flex items-center justify-center shadow-xl/20'>
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-[#ffff]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+          </svg>
+        </a> */}
         </div>
     </div>
   )
--- src/app/(landing)/_components/Search.tsx
+++ src/app/(landing)/_components/Search.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export function SearchInput() {
   return (
-    <div className="relative w-full max-w-7xl z-10">
+    <div className="relative w-full max-w-xl z-10 h-16">
       <input
-        type="text"
-        placeholder="Search..."
-        className="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500"
+      type="text"
+      placeholder="Search..."
+      className="w-full px-4 py-3 text-gray-700 bg-white border border-gray-300 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500 h-full pr-20"
       />
-     <div className='rounded-bl-full rounded-r-full h-full bg-gradient-to-b from-[#8EBD22] to-[#d0f47c] px-3 text-white p-2 absolute right-0 top-0'>
-        Rechercher
-     </div>
+      <button className="rounded-bl-full rounded-r-full h-full bg-gradient-to-b from-[#8EBD22] to-[#d0f47c] px-3 text-white absolute right-0 top-0 flex items-center justify-center">
+      Rechercher
+      </button>
     </div>
   );
 }
