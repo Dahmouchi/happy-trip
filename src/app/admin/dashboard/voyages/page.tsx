fe0347021e18b6d4bfc13874c4bf013e29801d4b
ybouyzem <bouyzemyounes2@gmail.com>
2025-06-09 16:58:29 +0100

    still to fix some bugs in services

--- src/app/admin/dashboard/voyages/page.tsx
+++ src/app/admin/dashboard/voyages/page.tsx
@@ -3,6 +3,7 @@ import VoyagesComponent from "../../_components/voyages-form";
 import { getDestinations } from "@/actions/destinations";
 import { getCategories } from "@/actions/categories";  
 import { getNatures } from "@/actions/natures";
+import { getServices } from "@/actions/services";
 
 
 
@@ -11,10 +12,11 @@ export default async function VoyagesPage() {
   const rawDestinations = await getDestinations();
   const rawCategories = await getCategories();
   const rawNatures = await getNatures();
+  const rawServices = await getServices();
 
   return (
     <div>
-      <VoyagesComponent initialDestinations={rawDestinations} initialCategories={rawCategories} initialNatures={rawNatures} />
+      <VoyagesComponent initialDestinations={rawDestinations} initialCategories={rawCategories} initialNatures={rawNatures}  initialServices={rawServices}/>
     </div>
   )
 }
